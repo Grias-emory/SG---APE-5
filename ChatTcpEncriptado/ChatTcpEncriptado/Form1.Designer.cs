188f49cc8b9234d2bdb5a916cdb06da5cd7798ae
FISEI-CTT <FISEI-CTT@UTA.EDU.EC>
2025-06-09 08:18:52 -0500

    Primer Commit

---
+++ ChatTcpEncriptado/ChatTcpEncriptado/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ChatTcpEncriptado
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnIniciarServidor;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtLog;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnIniciarServidor = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(150, 20);
+            this.txtIP.TabIndex = 0;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(168, 12);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(100, 20);
+            this.txtPuerto.TabIndex = 1;
+            this.txtPuerto.Text = "5000";
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(12, 240);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(350, 20);
+            this.txtMensaje.TabIndex = 2;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(274, 10);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(88, 23);
+            this.btnConectar.TabIndex = 3;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnIniciarServidor
+            // 
+            this.btnIniciarServidor.Location = new System.Drawing.Point(368, 10);
+            this.btnIniciarServidor.Name = "btnIniciarServidor";
+            this.btnIniciarServidor.Size = new System.Drawing.Size(110, 23);
+            this.btnIniciarServidor.TabIndex = 4;
+            this.btnIniciarServidor.Text = "Iniciar Servidor";
+            this.btnIniciarServidor.UseVisualStyleBackColor = true;
+            this.btnIniciarServidor.Click += new System.EventHandler(this.btnIniciarServidor_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(368, 237);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(110, 23);
+            this.btnEnviar.TabIndex = 5;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 40);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(466, 190);
+            this.txtLog.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(490, 270);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnIniciarServidor);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.txtPuerto);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.Text = "Chat TCP Encriptado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ ChatTcpEncriptado/ChatTcpEncriptado/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ChatTcpEncriptado
+{
+    public partial class Form1 : Form
+    {
+        TcpListener listener;
+        TcpClient client;
+        NetworkStream stream;
+        Thread recibirThread;
+        static string claveSecreta = "claveSegura16bts"; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarServidor_Click(object sender, EventArgs e)
+        {
+            int puerto = int.Parse(txtPuerto.Text);
+            listener = new TcpListener(IPAddress.Any, puerto);
+            listener.Start();
+            txtLog.AppendText("Servidor iniciado en puerto " + puerto + "\r\n");
+
+            Thread serverThread = new Thread(() =>
+            {
+                client = listener.AcceptTcpClient();
+                stream = client.GetStream();
+                this.Invoke((MethodInvoker)(() => txtLog.AppendText("Cliente conectado.\r\n")));
+
+                byte[] buffer = new byte[1024];
+                int bytesLeidos;
+                while ((bytesLeidos = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    string mensajeEncriptado = Encoding.UTF8.GetString(buffer, 0, bytesLeidos);
+                    string mensaje = Desencriptar(mensajeEncriptado);
+                    this.Invoke((MethodInvoker)(() => txtLog.AppendText("Recibido: " + mensaje + "\r\n")));
+                }
+            });
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect(txtIP.Text, int.Parse(txtPuerto.Text));
+                stream = client.GetStream();
+                txtLog.AppendText("Conectado al servidor.\r\n");
+
+                recibirThread = new Thread(() =>
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesLeidos;
+                    while ((bytesLeidos = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        string mensajeEncriptado = Encoding.UTF8.GetString(buffer, 0, bytesLeidos);
+                        string mensaje = Desencriptar(mensajeEncriptado);
+                        this.Invoke((MethodInvoker)(() => txtLog.AppendText("Recibido: " + mensaje + "\r\n")));
+                    }
+                });
+                recibirThread.IsBackground = true;
+                recibirThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al enviar: " + ex.Message);
+            }
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (stream != null && client.Connected && stream.CanWrite)
+            {
+                string mensaje = txtMensaje.Text;
+                string mensajeEncriptado = Encriptar(mensaje);
+                byte[] datos = Encoding.UTF8.GetBytes(mensajeEncriptado);
+                stream.Write(datos, 0, datos.Length);
+                txtLog.AppendText("Enviado: " + mensaje + "\r\n");
+                txtMensaje.Clear();
+            }
+        }
+
+        private string Encriptar(string texto)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                byte[] clave = Encoding.UTF8.GetBytes(claveSecreta);
+
+                // Asegura que la clave tenga 16 bytes (AES-128)
+                if (clave.Length != 16)
+                    throw new ArgumentException("La clave debe tener exactamente 16 caracteres para AES-128.");
+
+                aes.Key = clave;
+                aes.IV = new byte[16]; // Vector de inicialización fijo (puedes randomizar si deseas más seguridad)
+
+                ICryptoTransform encryptor = aes.CreateEncryptor();
+                byte[] inputBuffer = Encoding.UTF8.GetBytes(texto);
+                byte[] output = encryptor.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+                return Convert.ToBase64String(output);
+            }
+        }
+
+
+        private string Desencriptar(string textoEncriptado)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(claveSecreta);
+                aes.IV = new byte[16];
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                byte[] inputBuffer = Convert.FromBase64String(textoEncriptado);
+                byte[] output = decryptor.TransformFinalBlock(inputBuffer, 0, inputBuffer.Length);
+                return Encoding.UTF8.GetString(output);
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Lanzar dos ventanas automáticamente
+            Thread t1 = new Thread(() => Application.Run(new Form1()));
+            Thread t2 = new Thread(() => Application.Run(new Form1()));
+            t1.SetApartmentState(ApartmentState.STA);
+            t2.SetApartmentState(ApartmentState.STA);
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
